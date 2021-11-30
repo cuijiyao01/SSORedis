96f6375b282a18011194cafd01490d21e95ad6df
I531869 <jason.cui01@sap.com>
2021-11-30 14:38:30 +0800

    redisTemplate

--- ssoauth/src/main/java/com/hackday/service/UserService.java
+++ ssoauth/src/main/java/com/hackday/service/UserService.java
@@ -9,12 +9,15 @@ import com.hackday.utils.JsonUtils;
 import com.hackday.utils.Utils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.stereotype.Service;
 import org.springframework.util.StringUtils;
 
+import java.time.Duration;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.UUID;
+import java.util.concurrent.TimeUnit;
 
 /**
  * @Author i531869
@@ -29,6 +32,9 @@ public class UserService {
 
   @Autowired
   private JedisClient jedisClient;
+
+  @Autowired
+  private StringRedisTemplate stringRedisTemplate;
 
   @Value("${REDIS_USER_SESSION_KEY}")
   private String REDIS_USER_SESSION_KEY;
@@ -60,13 +66,12 @@ public class UserService {
     user.setPassword(null);
     user.setSalt(null);
     // 把用户信息写入 redis
-    jedisClient.set(REDIS_USER_SESSION_KEY + ":" + token, JsonUtils.objectToJson(user));
+    stringRedisTemplate.opsForValue()
+                       .set(REDIS_USER_SESSION_KEY + ":" + token, JsonUtils.objectToJson(user), SSO_SESSION_EXPIRE,
+                           TimeUnit.SECONDS);
     // user 已经是持久化对象了，被保存在了session缓存当中，若user又重新修改了属性值，那么在提交事务时，此时 hibernate对象就会拿当前这个user对象和保存在session缓存中的user对象进行比较，如果两个对象相同，则不会发送update语句，否则，如果两个对象不同，则会发出update语句。
     user.setPassword(userPassword);
     user.setSalt(userSalt);
-    // 设置 session 的过期时间
-    jedisClient.expire(REDIS_USER_SESSION_KEY + ":" + token, SSO_SESSION_EXPIRE);
-
     // 添加写 cookie 的逻辑，cookie 的有效期是关闭浏览器就失效。
     CookieUtils.setCookie(Utils.getReqAndRes().getLeft(), Utils.getReqAndRes().getRight(), "USER_TOKEN", token);
     // 返回token
@@ -75,13 +80,13 @@ public class UserService {
 
   public Result queryUserByToken(String token) {
     // 根据token从redis中查询用户信息
-    String json = jedisClient.get(REDIS_USER_SESSION_KEY + ":" + token);
+    String json = stringRedisTemplate.opsForValue().get(REDIS_USER_SESSION_KEY + ":" + token);
     // 判断是否为空
     if (StringUtils.isEmpty(json)) {
       return Result.build(400, "此session已经过期，请重新登录");
     }
     // 更新过期时间
-    jedisClient.expire(REDIS_USER_SESSION_KEY + ":" + token, SSO_SESSION_EXPIRE);
+    stringRedisTemplate.expire(REDIS_USER_SESSION_KEY + ":" + token, Duration.ofSeconds(SSO_SESSION_EXPIRE));
     // 返回用户信息
     return Result.ok(JsonUtils.jsonToPojo(json, User.class));
   }
